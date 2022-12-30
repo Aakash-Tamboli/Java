5d4a509b5d5b27a996e564ba929e6cf8cd9f34e3
Aakash Tamboli <athanhuntak@gmail.com>
2022-12-30 06:39:51 +0530

    Folder structure ready for InputUtility

---
+++ InputUtility/testcases/eg1.java
@@ -0,0 +1,16 @@
+import com.thinking.machines.utils.*;
+class psp
+{
+public static void main(String []args)
+{
+Keyboard k=new Keyboard();
+string name;
+char gender;
+int age;
+System.out.print("Name: ");
+name=k.getString(); // We need to implement this
+
+
+
+}
+}
