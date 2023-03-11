7c7e42d4d760d19d4e4d40eead2d8b703b333a77
Aakash-Tamboli <athanhuntak@gmail.com>
2023-03-11 12:17:29 +0530

    Environment ready for TMList

---
+++ TMList/README.md
@@ -0,0 +1,14 @@
+#	DOCUMENTATION
+
+## Overview
+here you get to know about `TMList` of My Java mini-projects.
+
+## Why The TMList in Java ?
+because I want to learn the internal implemenation of ArrayList which is part of collection classes, where I relate ArrayList as vector in c++, because I am curious person.
+
+## advantages and disadvantages of using array
+So internally at bottom level of java C/C++ are  used So I visualized that the address calculation theory works because I create my Own DsAlgo Library Where I found that array is abstract thing internally address calculation works,
+So Using array have biggest advantage that add and retrival of data is done O(1), but when it comes to insertion and deletion at particulat index then complexity will increased
+
+## LinkedList
+Using LinkedList we can resolve the issue of insertion and deletion at particular index, but Yes our O(1) is lost.
---
+++ TMList/srcOfTMArrayList.java
@@ -0,0 +1,115 @@
+class TMArrayList<T>
+{
+private Object[] collection;
+private int capacity;
+private int size;
+TMArrayList()
+{
+this.collection=new Object[10];
+this.size=0;
+this.capacity=10;
+}
+public void add(T data)
+{
+if(this.capacity==this.size)
+{
+Object []o;
+o=new Object[this.capacity*2];
+for(int i=0;i<this.size;i++)
+{
+o[i]=this.collection[i];
+}
+this.collection=o;
+this.capacity=this.capacity*2;
+}
+this.collection[this.size]=data;
+this.size++;
+}
+
+public void add(T data,int index)
+{
+if(index<0 || index > this.size) throw new IndexOutOfBoundsException("invalid index");
+if(this.capacity==this.size)
+{
+Object []o;
+o=new Object[this.capacity*2];
+for(int i=0;i<this.size;i++)
+{
+o[i]=this.collection[i];
+}
+this.collection=o;
+this.capacity=this.capacity*2;
+}
+for(int i=this.size;i>index;i--)
+{
+this.collection[i]=this.collection[i-1];
+}
+this.collection[index]=data;
+this.size++;
+}
+
+public T removeAt(int index)
+{
+if(index<0 || index>=this.size) throw new IndexOutOfBoundsException("Invalid index: "+index);
+for(int i=index;i<this.size;i++)
+{
+this.collection[i]=this.collection[i+1];
+}
+T data=(T)this.collection[index];
+this.size--;
+return data;
+}
+
+public int size()
+{
+return this.size;
+}
+public T get(int i)
+{
+if(i>=this.size) throw new IndexOutOfBoundsException("invalid index: "+i);
+else return (T)this.collection[i];
+}
+public int getCapacity()
+{
+return this.capacity;
+}
+public void clear()
+{
+// logically it is empty technically We still have buffer
+this.size=0;
+}
+}
+class psp
+{
+public static void main(String []args)
+{
+/*
+TMArrayList<Integer> list=new TMArrayList<Integer>();
+testcase for another case
+for(int i=0;i<list.size();i++) System.out.print(list.get(i)+" ");
+System.out.println("currently capacity: "+list.getCapacity());
+list.get(60000);
+for(int i=0;i<7;i++) list.add(i);
+list.add(790,5);
+for(int i=0;i<list.size();i++) System.out.print(list.get(i)+" ");
+System.out.println("Testing complete");
+for(int i=0;i<7;i++) list.add(i);
+System.out.println("currently capacity: "+list.getCapacity());
+list.clear();
+System.out.println("after clear method capacity is: "+list.getCapacity());
+System.out.println("after clear method size is: "+list.size());
+System.out.println("Trying to get data after clearing");
+System.out.println("Testing complete");
+for(int i=0;i<7;i++) list.add(i+1);
+System.out.println("removed: "+list.removeAt(3));
+for(int i=0;i<list.size();i++) System.out.print(list.get(i)+" ");
+System.out.println("Testing complete");
+*/
+TMArrayList<String> list=new TMArrayList<String>();
+list.add(new String("Aakash"));
+list.add("Bittu");
+list.add("AAKASH");
+for(int i=0;i<list.size();i++) System.out.print(list.get(i)+" ");
+
+}
+}
