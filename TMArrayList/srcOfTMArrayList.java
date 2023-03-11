324d16a4369c947b3407220b9b7a8930830cdfe6
Aakash-Tamboli <athanhuntak@gmail.com>
2023-03-11 11:28:54 +0530

    add overloaded as insertAt method

--- TMArrayList/srcOfTMArrayList.java
+++ TMArrayList/srcOfTMArrayList.java
@@ -25,6 +25,30 @@ this.capacity=this.capacity*2;
 this.collection[this.size]=data;
 this.size++;
 }
+
+
+public void add(T data,int index)
+{
+if(index<0 || index > this.size) throw new IndexOutOfBoundsException("invalid index");
+if(this.capacity==this.size)
+{
+Object []o;
+o=new Object[this.capacity*2];
+for(int i=0;i<this.size;i++)
+{
+o[i]=this.collection[i];
+}
+this.collection=o;
+this.capacity=this.capacity*2;
+}
+for(int i=this.size;i>index;i--)
+{
+this.collection[i]=this.collection[i-1];
+}
+this.collection[index]=data;
+this.size++;
+}
+
 public int size()
 {
 return this.size;
@@ -38,16 +62,35 @@ public int getCapacity()
 {
 return this.capacity;
 }
+public void empty()
+{
+// logically it is empty technically We still have buffer
+this.size=0;
+}
 }
 class psp
 {
 public static void main(String []args)
 {
 TMArrayList<Integer> list=new TMArrayList<Integer>();
-for(int i=0;i<500;i++) list.add(i);
+/*
+testcase for another case
 for(int i=0;i<list.size();i++) System.out.print(list.get(i)+" ");
 System.out.println("currently capacity: "+list.getCapacity());
 list.get(60000);
+for(int i=0;i<7;i++) list.add(i);
+list.add(790,5);
+for(int i=0;i<list.size();i++) System.out.print(list.get(i)+" ");
 System.out.println("Testing complete");
+*/
+for(int i=0;i<7;i++) list.add(i);
+System.out.println("currently capacity: "+list.getCapacity());
+list.empty();
+System.out.println("after empty method capacity is: "+list.getCapacity());
+System.out.println("after empty method size is: "+list.size());
+System.out.println("Trying to get data after clearing");
+list.get(0);
+System.out.println("Testing complete");
+
 }
 }
